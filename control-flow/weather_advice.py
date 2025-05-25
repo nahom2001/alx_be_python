bf92fa9cf2db8d68a49a363f52048a83c703618e
NahomH <nahomhulum@gmail.com>
2025-05-26 02:52:39 +0300

    edit prompt

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-weather = input('What\'s the weather today? (sunny/rainy/cold):.')
+weather = input("What's the weather like today? (sunny/rainy/cold):.")
 
 if weather == 'sunny':
     print('Wear a t-shirt and sunglasses.')
@@ -8,3 +8,5 @@ elif weather == 'cold':
     print('Make sure to wear a warm coat and a scarf.')
 else:
     print('Sorry, I don\'t have recommendations for this weather.')
+
+
