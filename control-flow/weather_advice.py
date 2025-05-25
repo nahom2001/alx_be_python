384952b122e32c48e74090ba2f3e0c9711eb67b0
NahomH <nahomhulum@gmail.com>
2025-05-26 02:38:56 +0300

    updating prompt

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-weather = input("What's the weather today? (sunny/rainy/cold): ")
+weather = input("What's the weather today? (sunny/rainy/cold):.")
 
 if weather == 'sunny':
     print('Wear a t-shirt and sunglasses.')
