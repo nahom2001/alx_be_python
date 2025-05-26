3b5bb081e698f09c0ff981b5b86c0cf943279126
NahomH <nahomhulum@gmail.com>
2025-05-26 04:26:00 +0300

    edit prompt

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
-time_bound = input("Is it time bound? (yes/no)")
+time_bound = input("Is it time bound? (yes/no): ")
 
 match priority:
     case 'high':
@@ -22,20 +22,5 @@ match priority:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-# print(priority)
-# if time_bound == 'yes':
-#     print(time_bound)
-# else:
-#     print(time_bound)
+input("Is it time bound (yes/no)")
+correction: /tmp/correction/1008188449509963505603933022569103408866_5/100740/648508/control-flow/daily_reminder.py doesn't contain input\s*\(\s*['\"]Is it time-bound\?\s*\(yes\/no\):\s*['\"]\s*\)
