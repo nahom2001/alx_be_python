91919944ad17ac15985523fec6d7538cdeefd38e
NahomH <nahomhulum@gmail.com>
2025-06-04 18:56:34 +0300

    correct formula use

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,12 +1,12 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9  # Correct factor for converting F to C
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  # Correct factor for converting C to F
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9  
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  
 
 def convert_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR  # Correct formula
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR  
     return celsius
 
 def convert_to_fahrenheit(celsius):
-    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32  # Correct formula
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32  
     return fahrenheit
 
 def main():
