7b88bdde10edb5bf4beef3c14aadb6128cac4b11
NahomH <nahomhulum@gmail.com>
2025-06-09 09:58:56 +0300

    instance methods

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,18 +4,20 @@ import unittest
 
 class TestCalculator(unittest.TestCase):
 
+    def setUp(self):
+        self.calculator = SimpleCalculator()
     def test_add(self):
-        result = SimpleCalculator.add(self, -2, 1)
+        result = self.calculator.add(self, -2, 1)
         self.assertEqual(result, -1)
 
     def test_subtract(self):
-        result = SimpleCalculator.subtract(self, -2, 1)
+        result = self.calculator.subtract(self, -2, 1)
         self.assertEqual(result, -3)
 
     def test_multiply(self):
-        result = SimpleCalculator.multiply(self, -2, -1)
+        result = self.calculator.multiply(self, -2, -1)
         self.assertEqual(result, 2)
 
     def test_divide(self):
-        result = SimpleCalculator.divide(self, 5, 2)
+        result = self.calculator.divide(self, 5, 2)
         self.assertEqual(result, 2.5)
