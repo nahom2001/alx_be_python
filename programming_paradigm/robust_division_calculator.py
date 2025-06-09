daa206bbf23095a546f4419e1e8415874ebdaa17
NahomH <nahomhulum@gmail.com>
2025-06-09 09:35:05 +0300

    using correct return statement

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,8 +4,9 @@ def safe_divide(numerator, denominator):
         return f"The result of the division is {result}"
 
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
+        numerator = float(n)
+        return "Error: Cannot divide by zero."
     except ValueError:
-        print("Error: Please enter numeric values only.")
+        return "Error: Please enter numeric values only."
 
 
