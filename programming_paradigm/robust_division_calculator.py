6df42d119df0cc11a6c5c203d01cf384457fd67c
NahomH <nahomhulum@gmail.com>
2025-06-09 09:36:33 +0300

    remove syntax error

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,7 +4,6 @@ def safe_divide(numerator, denominator):
         return f"The result of the division is {result}"
 
     except ZeroDivisionError:
-        numerator = float(n)
         return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please enter numeric values only."
