6b19267dc52a362e068084d7b4b412f125ce94a9
NahomH <nahomhulum@gmail.com>
2025-06-09 10:01:34 +0300

    renaming the instance

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,19 +5,19 @@ import unittest
 class TestCalculator(unittest.TestCase):
 
     def setUp(self):
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
     def test_addition(self):
-        result = self.calculator.add(self, -2, 1)
+        result = self.calc.add(self, -2, 1)
         self.assertEqual(result, -1)
 
     def test_subtraction(self):
-        result = self.calculator.subtract(self, -2, 1)
+        result = self.calc.subtract(self, -2, 1)
         self.assertEqual(result, -3)
 
     def test_multiply(self):
-        result = self.calculator.multiply(self, -2, -1)
+        result = self.calc.multiply(self, -2, -1)
         self.assertEqual(result, 2)
 
     def test_divide(self):
-        result = self.calculator.divide(self, 5, 2)
+        result = self.calc.divide(self, 5, 2)
         self.assertEqual(result, 2.5)
