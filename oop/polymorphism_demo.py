3a09ee5dd7e491f14afa71e61e9be71245c56025
NahomH <nahomhulum@gmail.com>
2025-06-19 15:20:27 +0300

    remove unnecessary arguments

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -9,7 +9,7 @@ class Rectangle(Shape):
         self.length = length
         self.width = width
 
-    def area(self, length, width):
+    def area(self):
         return self.length * self.width
 
 class Circle(Shape):
