c506089092618f7d4633cc4a820fde0671459c42
NahomH <nahomhulum@gmail.com>
2025-06-19 12:14:18 +0300

    removing the additional method

--- oop/book_class.py
+++ oop/book_class.py
@@ -10,8 +10,5 @@ class Book:
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
     
-    def __int__(self):
-        return self.year
-
     def __del__(self):
         print(f"Deleting {self.title}")
